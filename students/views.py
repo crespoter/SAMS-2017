d066c683e16f74e5bb82a0d6fe9928943954d966
crespoter <davidchristiek@gmail.com>
2017-11-01 14:42:16 +0530

    added index changes

--- students/views.py
+++ students/views.py
@@ -17,8 +17,66 @@ from django.utils import timezone
 @login_required(login_url="/login/")
 def dashboard(request):
     user = request.user;
-    print(user)
-    return render(request, "student/index.html", dict(name=user))
+
+
+    try:
+        user = request.user;                                                 
+        userPersonnelObj=Personnel.objects.filter(LDAP=user)
+        MyCourses = Students_Courses.objects.filter(Student_ID=userPersonnelObj[0].Person_ID); 
+        CourseAttendanceContext  = [];
+
+        for course in MyCourses:
+            AttendanceSessions = Attendance_Session.objects.filter(Course_Slot = course.Course_ID.Course_ID)
+            classesPresent = 0
+            totalClasses = 0
+            absentDays = []
+            totalClassesIncurrentMonth = 0;
+            totalClassesPresentInCurrentMonth = 0;
+            for sessions in AttendanceSessions:
+                try:
+                    attendanceObject = Attendance.objects.filter(Student_ID = userPersonnelObj[0].Person_ID).filter(ASession_ID=sessions.Session_ID)
+                    
+                    totalClasses += 1
+                    
+                    if(attendanceObject[0].Marked == 'P'):
+                        classesPresent += 1
+                        if(datetime.datetime.now().month == attendanceObject[0].Date_time.month):
+                            totalClassesIncurrentMonth+=1
+                            totalClassesPresentInCurrentMonth += 1                           
+
+                        
+                    elif(attendanceObject[0].Marked == 'A'):
+                        absentDays.append(attendanceObject[0].Date_time)
+                        if(datetime.datetime.now().month == attendanceObject[0].Date_time.month):
+                            totalClassesIncurrentMonth+=1
+
+                except:
+                    pass
+            if(totalClasses == 0):
+                retObj = dict(course=course,totalAttendance = "N.A",monthAttendance = "N.A")
+            elif(totalClassesIncurrentMonth == 0):
+                retObj = dict(course=course,totalAttendance = (classesPresent*100.0/totalClasses),monthAttendance = "N.A")
+            else:
+                retObj = dict(course=course,totalAttendance = (classesPresent*100.0/totalClasses),monthAttendance = (totalClassesPresentInCurrentMonth*100.0/totalClassesIncurrentMonth))
+            CourseAttendanceContext.append(retObj)
+        attendanceContext = dict(CourseAttendanceContext=CourseAttendanceContext)
+    except:
+        attendanceContext = dict(ErrorMessage = "No Registered Classes")
+    pendingAssignments = []
+    StudentObject=Personnel.objects.filter(LDAP=user.id)
+    CoursesByStudent = Students_Courses.objects.filter(Student_ID=StudentObject[0].Person_ID)
+    for course in CoursesByStudent:
+        AssignmentsForCourse = Assignment.objects.filter(Course_ID = course.Course_ID.Course_ID)
+        for assignment in AssignmentsForCourse:
+            submissionsByStudent = Submissions.objects.filter(Assign_ID = assignment).filter(Student_ID = StudentObject[0].Person_ID)
+            if(submissionsByStudent.count() == 0):
+                now = timezone.now()
+                if (assignment.End_Time > now):
+                    assignContextObject = dict(Course = course,assignment = assignment)
+                    pendingAssignments.append(assignContextObject)
+
+    
+    return render(request, "student/index.html", dict(name=user,attendanceContext=attendanceContext,assignmentContext = pendingAssignments))
 
 
 
